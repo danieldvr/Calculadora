dbe2ff41ee167a132f69a6183604fcab3b393e80
Sala 19A <sala19a@ceines.com.br>
2021-12-16 21:21:33 -0300

    Initial commit

---
+++ src/com/senac/Calculadora.java
@@ -0,0 +1,22 @@
+package com.senac;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+
+    double primeirovalor = 50.14;
+    double segundovalor = 11.21;
+
+
+    double soma = primeirovalor - segundovalor;
+    double subtração = primeirovalor - segundovalor;
+    double multiplicação = primeirovalor - segundovalor;
+    double divisão = primeirovalor - segundovalor;
+
+        System.out.println(" valor da soma = " + soma );
+        System.out.println(" valor da subtração = " + subtração );
+        System.out.println(" valor da multiplicação = " + multiplicação );
+        System.out.println(" valor da somadadivisão = " + divisão );
+
+    }
+}
